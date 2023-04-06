e8a73d1f72a0c89d388d14dc41d07a4cff519d45
Tomas Balcha <129552371+tomasbacha@users.noreply.github.com>
2023-04-06 12:00:10 +0300

    Create If else statement .cpp

---
+++ Chapter three each topic demostration/If else statement .cpp
@@ -0,0 +1,22 @@
+*one way if statement */
+// Program to print positive number entered by the user
+// If the user enters a negative number, it is skipped
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int number;
+
+  cout << "Enter an integer: ";
+  cin >> number;
+
+  // checks if the number is positive
+  if (number > 0) {
+    cout << "You entered a positive integer: " << number << endl;
+  }
+  
+
+  return 0;
+}
