1bc0b76ec4baf3f34f6985cc261bbbdc7c8b6e62
Tomas Balcha <129552371+tomasbacha@users.noreply.github.com>
2023-04-06 11:13:54 +0300

    Create exee1.cpp

---
+++ Chapter 2 exercises/exee1.cpp
@@ -0,0 +1,13 @@
+/*a program calculate the product of Three integers */
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, z, result;
+    cout<<" enter there members to multiply \n";
+    cin >>x>>y>>z;
+    result =x*y*z;
+    cout<<"the product is "<<result ;
+    return 0;
+}
