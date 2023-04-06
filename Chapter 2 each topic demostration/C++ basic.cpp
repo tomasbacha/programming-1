e365669de6a9f595e6a06e7ad774c190f7d83f4a
Tomas Balcha <129552371+tomasbacha@users.noreply.github.com>
2023-04-06 11:44:01 +0300

    Create C++ basic.cpp

---
+++ Chapter 2 each topic demostration/C++ basic.cpp
@@ -0,0 +1,19 @@
+/* this is a c++ code that operates the sum of two numbers that are taken from the key board */
+// including header file iostream
+#include <iostream> 
+
+using namespace std;
+
+int main() 
+{
+int x, y, sum; // this is variable decelaration and int is the keywoard
+sum=0; // assignining the value of sum
+cout<<"inter first number\n"; // output statement
+cin>>x; // input statement
+cout<<"inter second number\n";
+cin>>y;
+sum=x+y; 
+cout<<"the sum is "<<sum<<endl;
+
+return 0;
+}
