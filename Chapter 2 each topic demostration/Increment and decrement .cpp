b280ba9d0bae8e2e4b08f6422e4244dbafdbea3b
Tomas Balcha <129552371+tomasbacha@users.noreply.github.com>
2023-04-06 11:48:34 +0300

    Create Increment and decrement .cpp

---
+++ Chapter 2 each topic demostration/Increment and decrement .cpp
@@ -0,0 +1,19 @@
+/*increment and decrement operators*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 41, b = 84, result_a, result_b;
+
+    // incrementing a by 1 and storing the result in result_a
+    result_a = ++a;
+    cout << "result_a = " << result_a << endl;
+
+
+    // decrementing b by 1 and storing the result in result_b   
+    result_b = --b;
+    cout << "result_b = " << result_b << endl;
+
+    return 0;
+}
