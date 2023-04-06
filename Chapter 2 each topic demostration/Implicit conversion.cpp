58431ff494433c4294bf7d431cee6bfd57964773
Tomas Balcha <129552371+tomasbacha@users.noreply.github.com>
2023-04-06 11:46:49 +0300

    Create Implicit conversion.cpp

---
+++ Chapter 2 each topic demostration/Implicit conversion.cpp
@@ -0,0 +1,11 @@
+/*Implicit conversio*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a = 0.8;
+    float b = a; 
+    cout << "Value of b is: " << b << endl;
+    return 0;
+}
