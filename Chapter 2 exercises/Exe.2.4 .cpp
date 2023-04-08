6a064f489298b1276be36ce908889e6dc3192fa5
Tomas Balcha <129552371+tomasbacha@users.noreply.github.com>
2023-04-08 23:34:10 +0300

    Update Exe.2.4 .cpp

--- Chapter 2 exercises/Exe.2.4 .cpp
+++ Chapter 2 exercises/Exe.2.4 .cpp
@@ -1,36 +1,29 @@
-/* Roots of a Quadratic Equation*/
+//solve quadratic equation
 #include <iostream>
-#include <cmath>
+#include<cmath>
 using namespace std;
-
 int main() {
-
-    float a, b, c, x1, x2, discriminant, realPart, imaginaryPart;
-    cout << "Enter coefficients a, b and c: ";
-    cin >> a >> b >> c;
-    discriminant = b*b - 4*a*c;
-    
-    if (discriminant > 0) {
-        x1 = (-b + sqrt(discriminant)) / (2*a);
-        x2 = (-b - sqrt(discriminant)) / (2*a);
-        cout << "Roots are real and different." << endl;
-        cout << "x1 = " << x1 << endl;
-        cout << "x2 = " << x2 << endl;
+    int a,b,c,d;
+    float r1,r2;
+    cout<<"Enter the cofficients"<<endl;
+    cin>>a>>b>>c;
+    d=b*b-4*a*c;
+    if (d==0) {
+        cout<<"The roots are equal"<<endl;
+        r1=r2=-b/(2.0*a);
+        cout<<"The roots are r1=r2="<<r1<<endl;
     }
-    
-    else if (discriminant == 0) {
-        cout << "Roots are real and same." << endl;
-        x1 = -b/(2*a);
-        cout << "x1 = x2 =" << x1 << endl;
+    else if (d>0) {
+        cout<<"The roots are real and unequal"<<endl;
+        r1=(-b+sqrt(d))/(2.0*a);
+        r2=(-b-sqrt(d))/(2.0*a);
+        cout<<"r1="<<r1<<"r2="<<r2;
     }
-
     else {
-        realPart = -b/(2*a);
-        imaginaryPart =sqrt(-discriminant)/(2*a);
-        cout << "Roots are complex and different."  << endl;
-        cout << "x1 = " << realPart << "+" << imaginaryPart << "i" << endl;
-        cout << "x2 = " << realPart << "-" << imaginaryPart << "i" << endl;
+        cout<<"The roots are real and imaginery"<<endl;
+        r1=(-b+sqrt(abs(d))/2.0*a);
+        r2=(-b-sqrt(abs(d))/2.0*a);
+        cout<<"r1="<<r1<<"i and r2"<<r2<<"i";
     }
-
     return 0;
 }
