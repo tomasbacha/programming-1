ea15174cb88266823e755923f87c29200975af48
Tomas Balcha <129552371+tomasbacha@users.noreply.github.com>
2023-04-10 07:09:07 +0300

    Update If else if else statement .cpp

--- Chapter three each topic demostration/If else if else statement .cpp
+++ Chapter three each topic demostration/If else if else statement .cpp
@@ -1,25 +1,49 @@
-//multi way if-else
-// Program to check whether an integer is positive, negative or zero
-
+//multi way
 #include <iostream>
+
 using namespace std;
+int main()
 
-int main() {
+{
 
-  int x;
+ int mark;
 
-  cout << "Enter an integer: ";
-  cin >> x;
+ cout<<"Enter student's mark: ";
+ cin>>mark;
 
-  if (x > 0) {
-    cout << "You entered a positive integer: " << x << endl;
-  } 
-  else if (x < 0) {
-    cout << "You entered a negative integer: " << x << endl;
-  } 
-  else {
-    cout << "You entered 0." << endl;
-  }
+ 
 
-  return 0;
+ if (mark < 40)
+
+ cout<<"Student grade = F";
+
+ else if (mark < 50)
+
+ cout<<"Student grade = E";
+
+ else if (mark < 60)
+
+ cout<<"Student grade = D";
+
+ else if (mark < 70)
+
+ cout<<"Student grade = C";
+
+ else if (mark < 80)
+
+ cout<<"Student grade = B";
+
+ else
+
+ cout<<"Student grade = A";
+
+ cout<<"\n";
+
+ system("pause");
+
+return 0;
+
 }
+
+ 
+
