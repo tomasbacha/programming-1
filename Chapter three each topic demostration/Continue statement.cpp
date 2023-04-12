84372351e80d71ec489777c4c014ee83b28b5c11
Tomas Balcha <129552371+tomasbacha@users.noreply.github.com>
2023-04-12 18:49:09 +0300

    Create Continue statement.cpp

---
+++ Chapter three each topic demostration/Continue statement.cpp
@@ -0,0 +1,13 @@
+//continue
+#include <iostream>
+using namespace std;
+
+int main() {
+   for (int i=0; i < 10; i++) {
+      if (i==7) {
+         continue;
+      }
+      cout << i << "  ";
+   }
+   return 0;
+}
