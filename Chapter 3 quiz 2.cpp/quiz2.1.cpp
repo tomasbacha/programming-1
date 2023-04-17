aa0b347564abf2d22a8058c8710f821a270288b9
Tomas Balcha <129552371+tomasbacha@users.noreply.github.com>
2023-04-18 01:31:48 +0300

    Create quiz2.1.cpp

---
+++ Chapter 3 quiz 2.cpp/quiz2.1.cpp
@@ -0,0 +1,11 @@
+/*write the program that calculate sum of numbers from  1 to 100*/
+#include<iostream>
+using namespace std;
+int main(){
+	int sum =0;
+	for(int i=1; i<=100;  i++){
+		sum =sum+i;
+	}
+	cout<<sum<<endl;
+	return 0;
+}
