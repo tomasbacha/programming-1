2ad0317a7e53022088c22ed2685042c9eb920285
Tomas Balcha <129552371+tomasbacha@users.noreply.github.com>
2023-04-18 14:52:15 +0300

    Create String.cpp

---
+++ Chapter four each topic demonstration/String.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+/*	const int max=90;
+	char str1[ ]="Hello Ethiopia";
+	char str2[max];
+	int i;
+	for(i=0; i<strlen(str1); i++)
+	str2[i]=str1[i];
+	str2[i]='\0';
+	cout<<endl;
+	cout<<str2<<endl;
+*/
+/*
+		char me[20] ="Tomas";
+	cout<<me<<endl;
+	strcpy(me ,"i am c++ student");//strcpy is string copy function
+	cout<<me<<endl;
+	*/
+/*	char me[20]="Tomas";
+	cout <<me<<endl;
+	strncpy(me ,"I am c++ student" ,8);//strncpy is copies ony a specified number of character
+	cout<<me<<endl;
+	*/
+/*	char str1[26];
+	
+	strcpy(str1,"ABCD");
+	cout<<str1<<endl;
+	
+	strcat(str1,"EFGH");//The function strcat concatenate one string to the end of another string
+	cout<<str1<<endl;
+	
+	strcat(str1,"IJKL");
+	
+	char str2[ ]="XYZ";
+	
+	strcat(str1,str2);
+	cout<<str1<<endl;
+	str1[9]='\0';
+	cout<<str1<<endl;
+	*/
+
+/*	
+	//using strcmp conpare two string
+		cout<<strcmp("abcd","efgh")<<endl;
+	cout<<strcmp("ABCD","ABcdefh")<<endl;
+	cout<<strcmp("abcd","abcd")<<endl;
+	cout<<strcmp("abcdefh","abcd")<<endl;
+*/	
+
+/*	
+	//using strncmp is like strcmp expept that it compares only specified number of character.	
+	cout<<strncmp("abcd","efgh",4)<<endl;
+	cout<<strncmp("ABCD","ABcdefh",3)<<endl;
+	cout<<strncmp("abcd","abcd",2)<<endl;
+	cout<<strncmp("abcdefh","abcdE",5)<<endl;
+	*/
+	
+	return 0;
+}
